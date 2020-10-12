9550c31047ac060cfd933f3ef469102e44d60a29
Andrew Steinborn <git@steinborn.me>
2020-10-12 15:13:06 -0400

    Rehabilitate RawQueryResult (former SQLManager tuple functions).

--- HealthcareDBMS__/src/edu/westga/cs3230/healthcare_dbms/model/raw/RawQueryResult.java
+++ HealthcareDBMS__/src/edu/westga/cs3230/healthcare_dbms/model/raw/RawQueryResult.java
@@ -1,18 +1,40 @@
 package edu.westga.cs3230.healthcare_dbms.model.raw;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class RawQueryResult {
+
+	private List<String> columnNames;
+	private List<List<Object>> tuples;
 	
-	private List<Map<String, Object>> tuples;
-	
-	public RawQueryResult(List<Map<String, Object>> tuples) {
-		this.tuples = tuples;
+	public RawQueryResult(ResultSet set) throws SQLException {
+		this.columnNames = new ArrayList<>();
+
+		ResultSetMetaData metaData = set.getMetaData();
+		for (int i = 1; i <= metaData.getColumnCount(); i++) {
+			String columnName = metaData.getColumnName(i);
+			this.columnNames.add(columnName);
+		}
+
+		this.tuples = new ArrayList<>();
+		while (set.next()) {
+			List<Object> tuple = new ArrayList<>(this.columnNames.size());
+			for (int i = 1; i <= this.columnNames.size(); i++) {
+				tuple.add(set.getObject(i));
+			}
+			this.tuples.add(tuple);
+		}
 	}
 
-	public List<Map<String, Object>> getTuples() {
-		return this.tuples;
+	public List<String> getColumnNames() {
+		return columnNames;
 	}
 
+	public List<List<Object>> getTuples() {
+		return tuples;
+	}
 }
